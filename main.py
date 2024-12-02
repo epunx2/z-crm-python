9f17cd685fd7cf1d9d3e2ea9aaa155b218951f5f
Eric Chrobak <eric.chrobak@gmail.com>
2024-12-02 11:22:09 -0600

    add initializing Zoho client

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-# from dotenv import load_dotenv
 import logging
-
-# load_dotenv()
-
-logger = logging.getLogger()
+import sys
+sys.path.append('helpers')
+import helpers.zoho_crm_api as crm_api
 
 def lambda_handler(event, context):
   response = process()
   return response
 
 def process():
-  logger.warning("Running Process function")
   return "Hello World"
+
+def create_zoho_crm_client():
+  z_crm_api = crm_api.ZohoCrmApi()
+  logging.warning(z_crm_api)
+  return z_crm_api
 
 def main():
   response = process()
